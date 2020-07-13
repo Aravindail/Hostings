998fcec00a30a89c63dc104326d97dba5762d742
Danil Kuzmin <dazekamikadze19962002@gmail.com>
2020-07-13 10:42:23 +0300

    desctop

--- build/assets/scripts/script.js
+++ build/assets/scripts/script.js
@@ -1,10 +1,5 @@
-/*Унифицированные табы*/
-$(document).on('click', '.tabs-navigation-item', function (e) {
-	e.preventDefault();
-	let tabContainers = $(this).closest('.tabs').find('.tabs-tab');
-	tabContainers.removeClass('active');
-	tabContainers.filter(this.hash).addClass('active');;
-	$(this).closest('.tabs').find('.tabs-navigation-item').removeClass('active');
-	$(this).addClass('active');
+var mySwiper = new Swiper('.main-recommend-slider', {
+	slidesPerView: 'auto',
+	spaceBetween: 34,
+	freeMode: true
 });
-/*Унифицированные табы Конец*/
