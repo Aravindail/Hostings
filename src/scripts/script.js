359652f7b23d076742367c9fb37b7ca60b9405ff
Danil Kuzmin <dazekamikadze19962002@gmail.com>
2020-07-16 10:54:43 +0300

    adaptive

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,3 +3,10 @@ var mySwiper = new Swiper('.main-recommend-slider', {
 	spaceBetween: 34,
 	freeMode: true
 });
+
+var mySwiper = new Swiper('.main-comments-slider', {
+	slidesPerView: 'auto',
+	spaceBetween: 40,
+	freeMode: true
+});
+
